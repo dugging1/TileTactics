7afa413d7df150ea9c8224337f639e3dea4fbc17
TRBP <agentekul@googlemail.com>
2018-07-25 21:05:54 +0100

    UI Commit

---
+++ TileTactics/TileTactics/GUI.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TileTactics
+{
+    class GUI
+    {
+        public void draw(SpriteBatch s, Main m)
+        {
+            Vector2 vec = new Vector2(0, 1620);
+            s.Draw(Main.Textures["UI"], m.camera.ScreenToWorld(1620, 0));
+            //Vector2 t = new Vector2(vec.X * mat.m11 + vec.Y * mat.m12, vec.X * mat * m21 + vec.Y * mat * 22);
+        }
+    }
+}
--- TileTactics/TileTactics/Main.cs
+++ TileTactics/TileTactics/Main.cs
@@ -15,6 +15,7 @@ namespace TileTactics {
 		public Camera2D camera;
 		private RenderTarget2D rend;
 		private Map map;
+        private GUI gui;
 		public InputHandler inputHandler = new InputHandler();
 
 		public static Dictionary<string, Texture2D> Textures = new Dictionary<string, Texture2D>();
@@ -66,9 +67,10 @@ namespace TileTactics {
 			Textures.Add("Tile", Content.Load<Texture2D>("tile"));
             Textures.Add("OffAvatar", Content.Load<Texture2D>("AvatarOff"));
             Textures.Add("TileSelected", Content.Load<Texture2D>("tileselected"));
-
+            Textures.Add("UI", Content.Load<Texture2D>("UI"));
 
             map = new Map();
+            gui = new GUI();
 
 			rend = new RenderTarget2D(GraphicsDevice, Convert.ToInt32(Width), Convert.ToInt32(Height));
 
@@ -96,15 +98,14 @@ namespace TileTactics {
 				if(camera.BoundingRectangle.Left > 0) {
 					camera.Position -= new Vector2(cameraSpeed*dt.ElapsedGameTime.Milliseconds, 0);
 					if (camera.BoundingRectangle.Left < 0)
-						camera.Position = camera.WorldToScreen(0,0);
-						//camera.Position = new Vector2(0, camera.Position.Y);
+						camera.Position = new Vector2(0, camera.Position.Y);
 				}
 			}
 			if (inputHandler.isKeyPressed(Keys.D)) {
-				if (camera.BoundingRectangle.Right <= 70*64-480) {
+				if (camera.Position.X+960/camera.Zoom <= 70*64) {
 					camera.Position += new Vector2(cameraSpeed*dt.ElapsedGameTime.Milliseconds, 0);
-					if (camera.BoundingRectangle.Right > 70*64-480)
-						camera.Position = new Vector2(70*64-480, camera.Position.Y);
+					if (camera.Position.X+960/camera.Zoom > 70*64)
+						camera.Position = new Vector2(70*64-960/camera.Zoom, camera.Position.Y);
 				}
 			}
 			if (inputHandler.isKeyPressed(Keys.W)) {
@@ -115,10 +116,10 @@ namespace TileTactics {
 				}
 			}
 			if (inputHandler.isKeyPressed(Keys.S)) {
-				if (camera.BoundingRectangle.Bottom < 70*64-270) {
+				if (camera.Position.Y+540/camera.Zoom <= 70*64) {
 					camera.Position += new Vector2(0, cameraSpeed*dt.ElapsedGameTime.Milliseconds);
-					if (camera.BoundingRectangle.Bottom > 70*64-270)
-						camera.Position = new Vector2(camera.Position.X, 70*64-270);
+					if (camera.Position.Y+540/camera.Zoom > 70*64)
+						camera.Position = new Vector2(camera.Position.X, 70*64-540/camera.Zoom);
 				}
 			}
 			if (camera.Zoom + (inputHandler.deltaMWheelPos/1000.0f)/(GraphicsDevice.DisplayMode.Height/Height) < camera.MinimumZoom) {
@@ -135,13 +136,18 @@ namespace TileTactics {
 		/// <param name="gameTime">Provides a snapshot of timing values.</param>
 		protected override void Draw(GameTime gameTime) {
 			GraphicsDevice.Clear(Color.Black);
-
+			
 			GraphicsDevice.SetRenderTarget(rend);
 			GraphicsDevice.Clear(Color.Black);
 
 			spriteBatch.Begin(transformMatrix: camera.GetViewMatrix());
 			map.draw(spriteBatch, this);
+<<<<<<< HEAD
+            gui.draw(spriteBatch, this);
 
+=======
+			
+>>>>>>> 24e318606c836c05fb409f5b21cd757f8a002254
 			spriteBatch.End();
 
 			GraphicsDevice.SetRenderTarget(null);
