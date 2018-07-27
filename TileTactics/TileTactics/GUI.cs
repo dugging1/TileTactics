1dd3d50c03c72437ea5a39f61c7b42a913ebc780
dugging1 <goldminergaming@aol.com>
2018-07-27 02:38:29 +0100

    Formatting

--- TileTactics/TileTactics/GUI.cs
+++ TileTactics/TileTactics/GUI.cs
@@ -8,84 +8,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-<<<<<<< HEAD
 namespace TileTactics {
 	public class GUI {
 		public bool InfoOpen = false;
 		public bool ClickedInGUI = false;
-		public bool MainMenuOpen;
-=======
-namespace TileTactics
-{
-    public class GUI
-    {
-        public bool InfoOpen = false;
-        public bool ClickedInGUI = false;
-        public bool MainMenuOpen = true;
+		public bool MainMenuOpen = true;
 
-        // Mx = Mouse X, EntX = Top Left Entity X, Wx = Entity Width
-        public bool InBounds(float mx, float my, float entx, float enty, float wx, float wy)
-        {
-            if (mx > entx && mx < entx + wx)
-                if (my > enty && my < enty + wy)
-                    return true;
-            return false;
-        }
->>>>>>> 071ba9ad6c8d15f634f3d31ee115b21e9de7db88
+		// Mx = Mouse X, EntX = Top Left Entity X, Wx = Entity Width
+		public bool InBounds(float mx, float my, float entx, float enty, float wx, float wy) {
+			if (mx > entx && mx < entx + wx)
+				if (my > enty && my < enty + wy)
+					return true;
+			return false;
+		}
 
 		public void draw(SpriteBatch s, Main m) {
 			if (MainMenuOpen) {
 				Vector2 MMBackground = m.camera.ScreenToWorld(new Vector2(0, 0));
 
-                Vector2 MMIPBox = m.camera.ScreenToWorld(new Vector2(170, 104));
-                Vector2 MMPortBox = m.camera.ScreenToWorld(new Vector2(170, 160));
-                Vector2 MMNameBox = m.camera.ScreenToWorld(new Vector2(170, 234));
-                Vector2 MMPasswordBox = m.camera.ScreenToWorld(new Vector2(170, 307));
-
-                //Console.WriteLine(m.inputHandler.MousePos.X+" "+m.inputHandler.MousePos.X);
-
-                s.Draw(Main.Textures["MMBackground"], MMBackground, null, null, new Vector2(0, 0), 0, new Vector2(1 / m.camera.Zoom, 1 / m.camera.Zoom), Color.White);
+				Vector2 MMIPBox = m.camera.ScreenToWorld(new Vector2(170, 104));
+				Vector2 MMPortBox = m.camera.ScreenToWorld(new Vector2(170, 160));
+				Vector2 MMNameBox = m.camera.ScreenToWorld(new Vector2(170, 234));
+				Vector2 MMPasswordBox = m.camera.ScreenToWorld(new Vector2(170, 307));
 
 
-                if (InBounds(m.inputHandler.MousePos.X, m.inputHandler.MousePos.X, 170, 104, 168, 21))
-                {
-                    s.Draw(Main.Textures["MMTextOn"], MMIPBox, null, null, new Vector2(0, 0), 0, new Vector2(1 / m.camera.Zoom, 1 / m.camera.Zoom), Color.White);
-                }
-                else
-                {
-                    s.Draw(Main.Textures["MMTextOff"], MMIPBox, null, null, new Vector2(0, 0), 0, new Vector2(1 / m.camera.Zoom, 1 / m.camera.Zoom), Color.White);
-                }
+				s.Draw(Main.Textures["MMBackground"], MMBackground, null, null, new Vector2(0, 0), 0, new Vector2(1 / m.camera.Zoom, 1 / m.camera.Zoom), Color.White);
 
-                if (InBounds(m.inputHandler.MousePos.X, m.inputHandler.MousePos.X, 170, 160, 168, 21))
-                {
-                    s.Draw(Main.Textures["MMTextOn"], MMPortBox, null, null, new Vector2(0, 0), 0, new Vector2(1 / m.camera.Zoom, 1 / m.camera.Zoom), Color.White);
-                }
-                else
-                {
-                    s.Draw(Main.Textures["MMTextOff"], MMPortBox, null, null, new Vector2(0, 0), 0, new Vector2(1 / m.camera.Zoom, 1 / m.camera.Zoom), Color.White);
-                }
+				if (InBounds(m.inputHandler.MousePos.X, m.inputHandler.MousePos.X, 170, 104, 168, 21)) {
+					s.Draw(Main.Textures["MMTextOn"], MMIPBox, null, null, new Vector2(0, 0), 0, new Vector2(1 / m.camera.Zoom, 1 / m.camera.Zoom), Color.White);
+				} else {
+					s.Draw(Main.Textures["MMTextOff"], MMIPBox, null, null, new Vector2(0, 0), 0, new Vector2(1 / m.camera.Zoom, 1 / m.camera.Zoom), Color.White);
+				}
 
-                if (InBounds(m.inputHandler.MousePos.X, m.inputHandler.MousePos.X, 170, 234, 168, 21))
-                {
-                    s.Draw(Main.Textures["MMTextOn"], MMNameBox, null, null, new Vector2(0, 0), 0, new Vector2(1 / m.camera.Zoom, 1 / m.camera.Zoom), Color.White);
-                }
-                else
-                {
-                    s.Draw(Main.Textures["MMTextOff"], MMNameBox, null, null, new Vector2(0, 0), 0, new Vector2(1 / m.camera.Zoom, 1 / m.camera.Zoom), Color.White);
-                }
+				if (InBounds(m.inputHandler.MousePos.X, m.inputHandler.MousePos.X, 170, 160, 168, 21)) {
+					s.Draw(Main.Textures["MMTextOn"], MMPortBox, null, null, new Vector2(0, 0), 0, new Vector2(1 / m.camera.Zoom, 1 / m.camera.Zoom), Color.White);
+				} else {
+					s.Draw(Main.Textures["MMTextOff"], MMPortBox, null, null, new Vector2(0, 0), 0, new Vector2(1 / m.camera.Zoom, 1 / m.camera.Zoom), Color.White);
+				}
 
-                if (InBounds(m.inputHandler.MousePos.X, m.inputHandler.MousePos.X, 170, 307, 168, 21))
-                {
-                    s.Draw(Main.Textures["MMTextOn"], MMPasswordBox, null, null, new Vector2(0, 0), 0, new Vector2(1 / m.camera.Zoom, 1 / m.camera.Zoom), Color.White);
-                }
-                else
-                {
-                    s.Draw(Main.Textures["MMTextOff"], MMPasswordBox, null, null, new Vector2(0, 0), 0, new Vector2(1 / m.camera.Zoom, 1 / m.camera.Zoom), Color.White);
-                }
+				if (InBounds(m.inputHandler.MousePos.X, m.inputHandler.MousePos.X, 170, 234, 168, 21)) {
+					s.Draw(Main.Textures["MMTextOn"], MMNameBox, null, null, new Vector2(0, 0), 0, new Vector2(1 / m.camera.Zoom, 1 / m.camera.Zoom), Color.White);
+				} else {
+					s.Draw(Main.Textures["MMTextOff"], MMNameBox, null, null, new Vector2(0, 0), 0, new Vector2(1 / m.camera.Zoom, 1 / m.camera.Zoom), Color.White);
+				}
+
+				if (InBounds(m.inputHandler.MousePos.X, m.inputHandler.MousePos.X, 170, 307, 168, 21)) {
+					s.Draw(Main.Textures["MMTextOn"], MMPasswordBox, null, null, new Vector2(0, 0), 0, new Vector2(1 / m.camera.Zoom, 1 / m.camera.Zoom), Color.White);
+				} else {
+					s.Draw(Main.Textures["MMTextOff"], MMPasswordBox, null, null, new Vector2(0, 0), 0, new Vector2(1 / m.camera.Zoom, 1 / m.camera.Zoom), Color.White);
+				}
 
 			}
 
-<<<<<<< HEAD
 			if (m.inputHandler.isKeyDown(Keys.I)) {
 				if (InfoOpen == true) {
 					InfoOpen = false;
@@ -108,39 +82,6 @@ namespace TileTactics
 				Vector2 SelectedTitleLocV = m.camera.ScreenToWorld(vec4);
 				Vector2 vec5 = new Vector2(630 + 20, 0 + 140);
 				Vector2 SelectedTitleStaV = m.camera.ScreenToWorld(vec5);
-=======
-            if (m.inputHandler.isKeyDown(Keys.I))
-            {
-                if (InfoOpen == true)
-                {
-                    InfoOpen = false;
-                    ClickedInGUI = false;
-                }
-                else
-                {
-                    if (MainMenuOpen != true)
-                    {
-                        InfoOpen = true;
-                    }
-                }
-            }
-            if (m.inputHandler.isMBtnDown(0) && InfoOpen == true)
-            {
-                ClickedInGUI = true;
-            }
-            if (InfoOpen == true)
-            {
-                Vector2 vec1 = new Vector2(630, 0); //this makes me feel ill
-                Vector2 Backgroundloc = m.camera.ScreenToWorld(vec1);
-                Vector2 vec2 = new Vector2(630+20, 0+30);
-                Vector2 InfoTitleOffsetV = m.camera.ScreenToWorld(vec2);
-                Vector2 vec3 = new Vector2(630 + 20, 0 + 90);
-                Vector2 SelectedTitleV = m.camera.ScreenToWorld(vec3);
-                Vector2 vec4 = new Vector2(630 + 20, 0 + 115);
-                Vector2 SelectedTitleLocV = m.camera.ScreenToWorld(vec4);
-                Vector2 vec5 = new Vector2(630 + 20, 0 + 140);
-                Vector2 SelectedTitleStaV = m.camera.ScreenToWorld(vec5);
->>>>>>> 071ba9ad6c8d15f634f3d31ee115b21e9de7db88
 
 				Vector2 vec6 = new Vector2(630 + 20, 0 + 175);
 				Vector2 SelectedTitleStaNameV = m.camera.ScreenToWorld(vec6);
--- TileTactics/TileTactics/Map.cs
+++ TileTactics/TileTactics/Map.cs
@@ -43,66 +43,30 @@ namespace TileTactics {
 			}
 		}
 
-<<<<<<< HEAD
 		public void draw(SpriteBatch s, Main m) {
 			for (int x = 0; x < 70; x++) {
 				for (int y = 0; y < 70; y++) {
-					//Vector2 TempVector = m.camera.ScreenToWorld(m.inputHandler.MousePos) / 64;
-					if (TileSelected == new Vector2(x,y)) {
-						s.Draw(Main.Textures["TileSelected"], new Vector2((x * 64), (y * 64)));
-						//if (m.inputHandler.isMBtnPressed(0)) {
-						//	TileSelected = new Vector2(x, y);
-						//}
-					} else {
-						s.Draw(Main.Textures["Tile"], new Vector2((x * 64), (y * 64)));
-					}
-=======
-        public void draw(SpriteBatch s, Main m)
-        {
-            for (int x = 0; x < 70; x++)
-            {
-                for (int y = 0; y < 70; y++)
-                {
-                    Vector2 TempVector = m.camera.ScreenToWorld(m.inputHandler.MousePos) / 64;
-                    if (m.gui.MainMenuOpen != true)
-                    {
-                        if (Math.Floor(TempVector.X) == x && Math.Floor(TempVector.Y) == y)
-                        {
-                            s.Draw(Main.Textures["TileSelected"], new Vector2((x * 64), (y * 64)));
-                            if (m.inputHandler.isMBtnPressed(0))
-                            {
-                                TileSelected = new Vector2(x, y);
-                            }
-                        }
-                        else
-                        {
-                            s.Draw(Main.Textures["Tile"], new Vector2((x * 64), (y * 64)));
-                        }
-
-
-                        if (getData(x, y) != null)
-                        {
-                            getData(x, y).draw(s, new Vector2((x * 64), (y * 64)));
-                        }
-                    }
-                }
-            }
-        }
->>>>>>> 071ba9ad6c8d15f634f3d31ee115b21e9de7db88
-
-					if (getData(x, y) != null) {
-						getData(x, y).draw(s, new Vector2((x * 64), (y * 64)));
+					Vector2 TempVector = m.camera.ScreenToWorld(m.inputHandler.MousePos) / 64;
+					if (m.gui.MainMenuOpen != true) {
+						if (TileSelected == new Vector2(x,y)) {
+							s.Draw(Main.Textures["TileSelected"], new Vector2((x * 64), (y * 64)));
+						} else {
+							s.Draw(Main.Textures["Tile"], new Vector2((x * 64), (y * 64)));
+						}
+						if (getData(x, y) != null) {
+							getData(x, y).draw(s, new Vector2((x * 64), (y * 64)));
+						}
 					}
 				}
 			}
 		}
 
-		public bool MoveCheck(Vector2 Origin, Vector2 Endpoint) //checks radi of 1 tile around Origin
+		public bool MoveCheck(Vector2 Origin, Vector2 Endpoint) //checks square radi of 1 tile around Origin
 		{
 			return (Origin - Endpoint).Length() <= Math.Pow(2, 0.5f) && getData((int)Endpoint.X, (int)Endpoint.Y) == null;
 		}
 
-		public bool AimCheck(Vector2 Origin, Vector2 Endpoint) //checks radi of 3 tiles around Origin
+		public bool AimCheck(Vector2 Origin, Vector2 Endpoint) //checks square radi of 3 tiles around Origin
 		{
 			if (((Origin - Endpoint).Length() <= Math.Pow(18, 0.5f) && getData((int)Endpoint.X, (int)Endpoint.Y) == null))
 				if (Endpoint != Origin + new Vector2(0, 4) && Endpoint != Origin + new Vector2(4, 0) && Endpoint != Origin + new Vector2(-4, 0) && Endpoint != Origin + new Vector2(0, -4))
@@ -113,9 +77,6 @@ namespace TileTactics {
 		public bool MoveUnit(Vector2 CurrentLocation, Vector2 NewLocation) {
 			if (MoveCheck(CurrentLocation, NewLocation) && getData((int)CurrentLocation.X, (int)CurrentLocation.Y).AP >= 1) {
 				Client.ToSendPacket.Enqueue(new NetPacket(null, new MovePacket(CurrentLocation, NewLocation)));
-				//Unit Temp = getData((int)CurrentLocation.X, (int)CurrentLocation.Y);
-				//Temp.AP = Temp.AP - 1;
-				//setData((int)NewLocation.X, (int)NewLocation.Y, new Unit(Temp));
 				return true;
 			} else {
 				return false;
@@ -124,13 +85,6 @@ namespace TileTactics {
 
 		public bool Attack(Vector2 CurrentLocation, Vector2 EndLocation) {
 			if (getData((int)CurrentLocation.X, (int)CurrentLocation.Y).AP >= 1 && getData((int)EndLocation.X, (int)EndLocation.Y) != null) {
-				/*if (getData((int)EndLocation.X, (int)EndLocation.Y).HP == 1) {
-					setData((int)EndLocation.X, (int)EndLocation.Y, null);
-					//THIS IS NOT FINISHED - UPDATE TO HANDLE MOVING PLAYERS TO DEAD LIST!
-				} else {
-					getData((int)EndLocation.X, (int)EndLocation.Y).HP = getData((int)EndLocation.X, (int)EndLocation.Y).HP - 1;
-				}
-				getData((int)CurrentLocation.X, (int)CurrentLocation.Y).AP = getData((int)CurrentLocation.X, (int)CurrentLocation.Y).AP - 1;*/
 				Client.ToSendPacket.Enqueue(new NetPacket(null, new AttackPacket(CurrentLocation, EndLocation)));
 				return true;
 			} else {
@@ -141,8 +95,6 @@ namespace TileTactics {
 		public bool ShootAP(Vector2 CurrentLocation, Vector2 EndLocation) {
 			if (getData((int)CurrentLocation.X, (int)CurrentLocation.Y).AP >= 1 && getData((int)EndLocation.X, (int)EndLocation.Y) != null) {
 				Client.ToSendPacket.Enqueue(new NetPacket(null, new TradePacket(CurrentLocation, EndLocation)));
-				//getData((int)CurrentLocation.X, (int)CurrentLocation.Y).AP = getData((int)CurrentLocation.X, (int)CurrentLocation.Y).AP - 1;
-				//getData((int)EndLocation.X, (int)EndLocation.Y).AP = getData((int)EndLocation.X, (int)EndLocation.Y).AP + 1;
 				return true;
 			} else {
 				return false;
